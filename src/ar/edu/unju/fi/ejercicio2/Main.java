8cc114da1c8636183e4706e4e72950ce1216fe90
David Murga Pereyra <davidmurgapereyra@gmail.com>
2024-04-04 15:45:51 -0300

    Exercise 2 solved

---
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String pais = "España";
+        int edad = 30;
+        double alturaEdificio = 50.5; 
+        double precioProducto = 2.75; 
+        String numeroTelefono = "123456789";
+        double cosenoDe05 = Math.cos(0.5);
+
+        System.out.println("Nombre del país: " + pais);
+        System.out.println("Edad de una persona: " + edad + " años");
+        System.out.println("Altura de un edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio de un producto de supermercado: " + precioProducto + " euros");
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Cálculo de coseno de 0.5: " + cosenoDe05);
+	}
+
+}
