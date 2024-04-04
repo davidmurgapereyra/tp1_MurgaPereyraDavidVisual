87ce2f0ab20326c8a477d289f0b44f6d72db166b
David Murga Pereyra <davidmurgapereyra@gmail.com>
2024-04-04 15:38:57 -0300

    Exercice 1 solved

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 6;
+        int num2 = 9;
+        int num3 = 4;
+        int num4 = 13;
+        int num5 = 9;
+
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+
+        System.out.println("El promedio es: " + promedio);
+	}
+
+}
