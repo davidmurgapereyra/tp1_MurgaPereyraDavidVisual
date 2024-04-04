723f64c3fc9bf502f96e670ff79394ce2c704f95
David Murga Pereyra <davidmurgapereyra@gmail.com>
2024-04-04 16:48:40 -0300

    Exercise 5 solved

---
+++ src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entre 1 y 9 para mostrar su tabla de multiplicar: ");
+        int numero = scanner.nextInt();
+        
+        if (numero < 1 || numero > 9) {
+            System.out.println("El número debe ser entre 1 y 9.");
+        } else {
+            System.out.println("Tabla de multiplicar del " + numero + ":");
+
+            for (int i = 1; i <= 10; i++) {
+                int resultado = numero * i;
+                System.out.println(numero + " x " + i + " = " + resultado);
+            }
+        }
+        scanner.close();
+    }
+	
+
+}
